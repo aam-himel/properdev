8cc26b150138ce6222d43c86277d1bf1a322a670
aamhimel <aam.himel@gmail.com>
2021-08-03 04:46:04 +0600

    testimonial card design@Testimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,10 +1,34 @@
 import React from "react";
+import manImg from "../../assets/img/man.jpg";
+import quoteImg from "../../assets/img/quote.svg";
+import "./Testimonial.css";
 
 const Testimonial = () => {
   return (
-    <div>
-      <h1>testimonial</h1>
-    </div>
+    <section className="h-screen w-full bg-blue-50 px-10 flex items-center justify-center">
+      <div className="testimonial_card bg-white shadow-xl grid grid-cols-6 items-center content-center gap-3 p-8">
+        <div className=" overflow-hidden col-span-2">
+          <img src={manImg} alt="man" className="px-2 " />
+        </div>
+        <img
+          src={quoteImg}
+          alt="quote"
+          className="h-48 col-span-1 opacity-10"
+        ></img>
+        <div className="col-span-2 p-4">
+          <p className="leading-7 tracking-wide font-semibold">
+            This is what I have been looking for a long time. I am amazed how
+            good this platform is !
+          </p>
+        </div>
+        <img
+          src={quoteImg}
+          alt="quote"
+          className="h-48 transform rotate-180 col-span-1 my-40 opacity-10"
+        ></img>
+        <div className="slider-right"></div>
+      </div>
+    </section>
   );
 };
 
---
+++ src/utils/fake-data/testimonials.js
@@ -0,0 +1,28 @@
+const testimonials = [
+  {
+    id: 100,
+    clientName: "Abdullah",
+    comment:
+      "This is what I have been looking for a long time. I am amazed how good this platform is !",
+  },
+  {
+    id: 100,
+    clientName: "Abdullah",
+    comment:
+      "This is what I have been looking for a long time. I am amazed how good this platform is !",
+  },
+  {
+    id: 100,
+    clientName: "Abdullah",
+    comment:
+      "This is what I have been looking for a long time. I am amazed how good this platform is !",
+  },
+  {
+    id: 100,
+    clientName: "Abdullah",
+    comment:
+      "This is what I have been looking for a long time. I am amazed how good this platform is !",
+  },
+];
+
+export { testimonials };
